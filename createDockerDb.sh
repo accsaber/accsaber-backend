bd838f69b5f9a7821a2f6fdb7558b4f42907dc3a
Ixsen <ixsen@mailbox.org>
2021-01-15 20:48:17 +0000

    bugfix/migrations_max-score (#8)

--- createDockerDb.sh
+++ createDockerDb.sh
@@ -2,7 +2,7 @@ docker run \
     -p 3306:3306 \
     --name accsaber-db \
     -e MYSQL_ROOT_PASSWORD=root \
-    -e MYSQL_DATABASE=acc \
+    -e MYSQL_DATABASE=accsaber_db \
     -e MYSQL_USER=acc \
     -e MYSQL_PASSWORD=acc \
     -d mariadb:10.5 \
