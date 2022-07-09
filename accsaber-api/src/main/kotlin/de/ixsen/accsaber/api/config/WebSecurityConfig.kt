fb72b6b8500257f1a5af248111347ff511330617
Ixsen <ixsen@mailbox.org>
2022-07-09 14:41:41 +0200

    Updated dependencies, removed deprecated WebSecurityConfigurerAdapter

--- accsaber-api/src/main/kotlin/de/ixsen/accsaber/api/config/WebSecurityConfig.kt
+++ accsaber-api/src/main/kotlin/de/ixsen/accsaber/api/config/WebSecurityConfig.kt
@@ -1,29 +1,34 @@
 package de.ixsen.accsaber.api.config
 
-import de.ixsen.accsaber.business.AccSaberUserDetailsService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
+
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfig(
-    private val userDetailsService: AccSaberUserDetailsService,
-    private val bCryptPasswordEncoder: PasswordEncoder,
     @param:Value("\${accsaber.auth.secret}") private val secret: String,
     @param:Value("\${accsaber.auth.expiration-time}") private val expirationTime: Long
-) : WebSecurityConfigurerAdapter() {
-    @Throws(Exception::class)
-    override fun configure(http: HttpSecurity) {
+) {
+
+    @Bean
+    @Throws(java.lang.Exception::class)
+    fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager? {
+        return authenticationConfiguration.authenticationManager
+    }
+
+    @Bean
+    fun filterChain(http: HttpSecurity, authenticationManager: AuthenticationManager): SecurityFilterChain {
         http.cors().and().csrf().disable()
             .authorizeRequests()
             .antMatchers(HttpMethod.GET, "/staff/**").authenticated()
@@ -35,13 +40,10 @@ class WebSecurityConfig(
             .antMatchers(HttpMethod.DELETE, "/**").authenticated()
             .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
             .and()
-            .addFilter(AuthenticationFilter(authenticationManager(), secret, expirationTime))
-            .addFilter(AuthorizationFilter(authenticationManager(), secret))
-    }
+            .addFilter(AuthenticationFilter(authenticationManager, secret, expirationTime))
+            .addFilter(AuthorizationFilter(authenticationManager, secret))
 
-    @Throws(Exception::class)
-    public override fun configure(auth: AuthenticationManagerBuilder) {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder)
+        return http.build()
     }
 
     @Bean
